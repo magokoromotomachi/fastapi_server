e0f30c969cb909ad84fc529f6f603bc04f166fb6
magokoromotomachi <magokoromotomachi@gmail.com>
2023-05-22 02:29:13 +0900

    geojsonを配信する

---
+++ __pycache__/README.md
@@ -0,0 +1,18 @@
+# FASTAPI SERVER
+
+# package install
+
+```Console
+$ pip install fastapi
+$ pip install uvicorn
+```
+
+# server start
+
+```Console
+$ uvicorn main:app --reload
+
+or
+
+$ python -m uvicorn main:app --reload
+```
--- main.py
+++ main.py
@@ -3,32 +3,22 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
-from fastapi import APIRouter
 
-# router = APIRouter()
+
 app = FastAPI()
-# app.include_router(router)
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 def root(request: Request):
-    return templates.TemplateResponse(
-        'index.html',
-        {
-            'request': request
-        }
-    )
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get('/maps/{map_id}', response_class=HTMLResponse)
+@app.get("/maps/{map_id}", response_class=HTMLResponse)
 def get_map(map_id: str, request: Request):
     return templates.TemplateResponse(
-        'map.html',
-        {
-            'request': request,
-            'map_id': map_id
-        }
+        "map.html",
+        {"request": request, "map_id": map_id, "geojson_path": f"/json/{map_id}.json"},
     )
