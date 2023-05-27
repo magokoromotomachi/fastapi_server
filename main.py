d8633efe2a980514993d114c5c25e180f23c0c9f
magokoromotomachi <magokoromotomachi@gmail.com>
2023-05-27 21:21:35 +0900

    GitHub Pagesで静的ページを公開

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.requests import Request
 from fastapi.staticfiles import StaticFiles
+from make_geojson import tokyo_ku_dict
 
 
 app = FastAPI()
@@ -13,7 +14,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request, "tokyo_ku_dict": tokyo_ku_dict})
 
 
 @app.get("/maps/maplibre.html", response_class=HTMLResponse)
@@ -26,7 +27,9 @@ def get_map(request: Request):
 
 @app.get("/maps/{map_id}", response_class=HTMLResponse)
 def get_map(map_id: str, request: Request):
+    title = tokyo_ku_dict[map_id.split('_')[0]]
+    geojson_path = f"/static/json/{map_id.replace('.html', '')}.geojson"
     return templates.TemplateResponse(
         "map.html",
-        {"request": request, "map_id": map_id, "geojson_path": f"/json/{map_id}.geojson"},
+        {"request": request, "title": title, "geojson_path": geojson_path},
     )
